4e273d26f525ef982c55348e9ef3a0f1ba70510d
pelander <pellewisten@gmail.com>
2014-10-29 19:21:09 -0700

    try added weather descpriottion

--- js/sketch.js
+++ js/sketch.js
@@ -63,10 +63,21 @@ function hexToRgb(hex) {
 }
 
 function gotWeather(weather) {
+<<<<<<< Updated upstream
+=======
+  
+>>>>>>> Stashed changes
   // Get the wind speed
   var windmag = Number(weather.wind.speed);
+  var winddesc = floor(weather.main.description);
   
   // Display as HTML elements
+<<<<<<< Updated upstream
   var temperatureDiv = createDiv(floor(weather.main.temp) + '&deg;').addClass('text1');
   var windDiv = createDiv("WIND " + windmag + " <small>MPH</small>").addClass('text2');
+=======
+  var temperatureDiv = createDiv(floor(weather.main.temp) + '&deg;');
+  var windDiv = createDiv("WIND " + windmag + " <small>MPH</small>");
+  var descriptionDiv = createDiv("Weather " + winddesc + " <small>Hej</small>");
+>>>>>>> Stashed changes
 }
