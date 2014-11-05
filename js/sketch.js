5d776937a5a9ddbd9a04779b9308013fb7b581aa
scarsam <samojling@Sams-MacBook-Pro.local>
2014-11-04 17:14:34 -0800

    latest version

--- js/sketch.js
+++ js/sketch.js
@@ -1,37 +1,52 @@
 var Y_AXIS = 1;
-var ba1, ba2;
+var PartlyCloudy1, PartlyCloudy2, Sunny1, Sunny2, Cloudy1, Cloudy2, Snowing1, Snowing2, Rain1, Rain2, Fog1, Fog2;
 var weather;
 var loaded;
 var main;
+var name;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  ba1 = color(221,214,243);
-  ba2 = color(250,172,168);
+  PartlyCloudy1 = color(218,226,248);
+  PartlyCloudy2 = color(214,164,164);
+
+  Sunny1 = color(255,78,80);
+  Sunny2 = color(249,212,35);
+
+  Cloudy1 = color(236,233,230);
+  Cloudy2 = color(255,255,255);
+
+  Snowing1 = color(230,218,218);
+  Snowing2 = color(39,64,70);
+
+  Rain1 = color(97,97,97);
+  Rain2 = color(155,197,195);
+
+  Fog1 = color(117,127,154);
+  Fog2 = color(215,221,232);      
 
   // Request the data from openweathermap
-  // loadJSON('http://api.openweathermap.org/data/2.5/weather?q=SanFrancisco,US', gotWeather);
   loadJSON('http://api.openweathermap.org/data/2.5/weather?q=San%20Francisco,CA&units=imperial', function(w){
         loaded = true;
         weather = w.weather;
         main = w.main;
+        name = w.name;
         createDiv( floor(main.temp) + '&deg;').addClass('temperature');
-        createDiv( text(weather.description)).addClass('wind');
+        createDiv( weather[0].main).addClass('wind');
+        createDiv( name).addClass('wind');
         print(weather);
         print(main);
+        print(name);
     });
 }
 
-
-
 function draw() {
-  setGradient(0, 0, width/2, height, ba1, ba2, 1);
+  setGradient(0, 0, width/2, height, PartlyCloudy1, PartlyCloudy2, 1);
 
 }
 
 function setGradient(x, y, w, h, c1, c2, axis) {
-
   noFill();
 
   for (var i = y; i <= y+h; i++) {
@@ -40,10 +55,4 @@ function setGradient(x, y, w, h, c1, c2, axis) {
       stroke(c);
       line(x, i, x+w, i);
     }
-}
-
-function w(weather) {
-
-  
-  // Display as HTML elements
 }
