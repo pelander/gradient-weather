5ad6b62aec50a608b957e01f3430b6aa4971d949
Pelle Wisten <pellewisten@gmail.com>
2014-11-06 10:59:44 -0800

    Fixed sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -23,8 +23,23 @@ function setup() {
   Rain1 = color(97,97,97);
   Rain2 = color(155,197,195);
 
-  Fog1 = color(117,127,154);
-  Fog2 = color(215,221,232);      
+  Haze1 = color(117,127,154);
+  Haze2 = color(215,221,232); 
+
+  Fog1 = color(255,255,255);
+  Fog2 = color(236,233,230);
+
+  Thunder1 = color(35,37,38);
+  Thunder2 = color(65,67,69);
+
+  Moreclouds1 = color(117,127,154);
+  Moreclouds2 = color(215,221,232);
+
+
+
+
+  Fewclouds1 = color();
+  Fewclouds2 = color();     
 
   // Request the data from openweathermap
   loadJSON('http://api.openweathermap.org/data/2.5/weather?q=San%20Francisco,CA&units=imperial', function(w){
@@ -33,22 +48,34 @@ function setup() {
         main = w.main;
         name = w.name;
         createDiv( floor(main.temp) + '&deg;').addClass('temperature');
-        createDiv( weather[0].main).addClass('wind');
+        createDiv( weather[0].description).addClass('wind');
         createDiv( name).addClass('wind');
         print(weather);
         print(main);
         print(name);
+        print(weather[0].id)
     });
 }
 
 function draw() {
-  setGradient(0, 0, width/2, height, PartlyCloudy1, PartlyCloudy2, 1);
+  if(loaded){
 
+  	
+
+    if( weather[0].id >= 200 && weather[0].id <= 232 ) setGradient(0, 0, width, height, Thunder1, Thunder2, 1); 
+    if( weather[0].id >= 300 && weather[0].id <= 331 ) setGradient(0, 0, width, height, Rain1, Rain2, 1);
+    if( weather[0].id >= 500 && weather[0].id <= 531 ) setGradient(0, 0, width, height, Rain1, Rain2, 1);
+    if( weather[0].id >= 600 && weather[0].id <= 622 ) setGradient(0, 0, width, height, Snowing1, Snowing2, 1);
+    if( weather[0].id >= 701 && weather[0].id <= 731 ) setGradient(0, 0, width, height, Haze1, Haze2, 1);
+    if( weather[0].id >= 741 && weather[0].id <= 781 ) setGradient(0, 0, width, height, Fog1, Fog2, 1);
+    if( weather[0].id == 800 ) setGradient(0, 0, width, height, Sunny1, Sunny2, 1);
+    if( weather[0].id == 801 ) setGradient(0, 0, width, height, PartlyCloudy2, PartlyCloudy1, 1);
+    if( weather[0].id >= 802 && weather[0].id <= 804 ) setGradient(0, 0, width, height, Moreclouds1, Moreclouds2, 1);
+
+  }
 }
 
 function setGradient(x, y, w, h, c1, c2, axis) {
-  noFill();
-
   for (var i = y; i <= y+h; i++) {
       var inter = map(i, y, y+h, 0, 1);
       var c = lerpColor(c1, c2, inter);
@@ -56,3 +83,34 @@ function setGradient(x, y, w, h, c1, c2, axis) {
       line(x, i, x+w, i);
     }
 }
+
+
+// if (weather[0].id >= 200 && weather[0].id <= 232) {
+//   setGradient(0, 0, width, height, Sunny1, Sunny2, 1);
+// } else if( weather[0].id >= 300 && weather[0].id <= 331 ) { 
+//   setGradient(0, 0, width, height, Rain1, Rain2, 1);
+// } else if( weather[0].id >= 500 && weather[0].id <= 531 ) {
+// setGradient(0, 0, width, height, Rain1, Rain2, 1);
+// } else if( weather[0].id >= 600 && weather[0].id <= 622 ) {
+// setGradient(0, 0, width, height, Snowing1, Snowing2, 1);
+// }
+
+// if ((weather[0].id >= 200) || (weather[0].id <= 232)) {
+//       setGradient(0, 0, width, height, Sunny1, Sunny2, 1);
+//     } else if( (weather[0].id >= 300) || (weather[0].id <= 331)) { 
+//       setGradient(0, 0, width, height, Rain1, Rain2, 1);
+//     } else if( (weather[0].id >= 500) || (weather[0].id <= 531)) {
+//     setGradient(0, 0, width, height, Rain1, Rain2, 1);
+//     } else if( (weather[0].id >= 600) || (weather[0].id <= 622)) {
+//     setGradient(0, 0, width, height, Snowing1, Snowing2, 1);
+//     }
+
+// if( weather[0].id >= 200 || weather[0].id <= 232 ) setGradient(0, 0, width, height, Sunny1, Sunny2, 1);
+// if( weather[0].id >= 300 || weather[0].id <= 331 ) setGradient(0, 0, width, height, Rain1, Rain2, 1);
+// if( weather[0].id >= 500 || weather[0].id <= 531 ) setGradient(0, 0, width, height, Rain1, Rain2, 1);
+// if( weather[0].id >= 600 || weather[0].id <= 622 ) setGradient(0, 0, width, height, Snowing1, Snowing2, 1);
+
+// if( weather[0].id >= 200 && weather[0].id <= 232 ) setGradient(0, 0, width, height, Sunny1, Sunny2, 1);
+// if( weather[0].id >= 300 && weather[0].id <= 331 ) setGradient(0, 0, width, height, Rain1, Rain2, 1);
+// if( weather[0].id >= 500 && weather[0].id <= 531 ) setGradient(0, 0, width, height, Rain1, Rain2, 1);
+// if( weather[0].id >= 600 && weather[0].id <= 622 ) setGradient(0, 0, width, height, Snowing1, Snowing2, 1);
